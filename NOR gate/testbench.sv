de08565dc0e9ec40cf6c77ab2e620e4bbeb5af33
Anusmita Dasgupta <anusmitadasgupta1305@gmail.com>
2025-12-25 13:34:23 +0530

    Project files uploaded

---
+++ NOR gate/testbench.sv
@@ -0,0 +1,32 @@
+`timescale 1ns/1ps
+
+module or_tb;
+  
+reg a;
+reg b;
+wire y;
+  
+  or_gate dut(
+  
+    .a(a),
+    .b(b),
+    .y(y)
+    
+  );
+  
+  initial begin
+  
+  a = 0; b = 0;
+    $display("%b", y); 
+  #5 a = 1; b = 0;
+     $display("%b", y); 
+  #5 a = 0; b = 1;
+     $display("%b", y); 
+  #5 a = 1; b = 1;
+     $display("%b", y); 
+  #5 $finish;
+    
+  end
+  
+  
+endmodule
