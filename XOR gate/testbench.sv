9c0edaee17d13fa52445d9464f23dd8ce8389093
Anusmita Dasgupta <anusmitadasgupta1305@gmail.com>
2025-12-25 13:36:30 +0530

    Project files uploaded

---
+++ XOR gate/testbench.sv
@@ -0,0 +1,32 @@
+`timescale 1ns/1ps
+
+module xor_tb;
+  
+reg a;
+reg b;
+wire y;
+  
+  xor_gate dut(
+  
+    .a(a),
+    .b(b),
+    .y(y)
+    
+  );
+  
+  initial begin
+  
+  a = 0; b = 0;
+    $display("%b", y); 
+  #5 a = 1; b = 0;
+     $display("%b", y); 
+  #5 a = 0; b = 1;
+     $display("%b", y); 
+  #5 a = 1; b = 1;
+     $display("%b", y); 
+  #5 $finish;
+    
+  end
+  
+  
+endmodule
